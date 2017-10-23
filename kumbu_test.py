8f09e1159fd2d5ffb8e27135975572543acf411f
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-23 19:48:39 +0530

    Use fixture for webdriver

--- kumbu_test.py
+++ kumbu_test.py
@@ -1,97 +1,96 @@
 from selenium import webdriver
 import time
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_l001():
-    driver = webdriver.Chrome()
+@pytest.mark.usefixtures('driver')
+class TestKumbuFunctional:
+    @pytest.fixture(scope='class')
+    def driver(self):
+        driver = webdriver.Chrome()
+        yield driver
+        driver.quit()
 
-    driver.get('https://staging.getkumbu.com')
-    driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
-    driver.find_element_by_name('inputPassword').send_keys('$PASSWORD')
-    driver.find_element_by_id('login-submit').click()
+    def test_l001(self, driver):
+        driver.get('https://staging.getkumbu.com')
+        driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
+        driver.find_element_by_name('inputPassword').send_keys('$PASSWORD')
+        driver.find_element_by_id('login-submit').click()
 
-    links = driver.find_elements_by_class_name('profile-link')
-    assert len(links) != 0 and 'Kumbu Test M2' in links[0].text
-    links[0].click()
+        links = driver.find_elements_by_class_name('profile-link')
+        assert len(links) != 0 and 'Kumbu Test M2' in links[0].text
+        links[0].click()
 
-    '''Profile Panel is not implemented yet!'''
+        '''Profile Panel is not implemented yet!'''
 
+    def test_l002(self, driver):
+        driver.get('https://staging.getkumbu.com')
+        driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
+        driver.find_element_by_name('inputPassword').send_keys('kumbu ​is ​not ​cool')
+        driver.find_element_by_id('login-submit').click()
 
-def test_l002():
-    driver = webdriver.Chrome()
+        flashes = driver.find_elements_by_id('flash-messages')
+        assert len(flashes) != 0 and 'Invalid email or password' in flashes[0].text
+        flashes[0].find_element_by_class_name('close-button').click()
 
-    driver.get('https://staging.getkumbu.com')
-    driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
-    driver.find_element_by_name('inputPassword').send_keys('kumbu ​is ​not ​cool')
-    driver.find_element_by_id('login-submit').click()
+    def test_l003(self, driver):
+        driver.get('https://staging.getkumbu.com')
+        driver.find_element_by_class_name('password-link').click()
+        assert 'https://staging.getkumbu.com/reset' in driver.current_url
 
-    flashes = driver.find_elements_by_id('flash-messages')
-    assert len(flashes) != 0 and 'Invalid email or password' in flashes[0].text
-    flashes[0].find_element_by_class_name('close-button').click()
+        driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
+        driver.find_element_by_id('login-submit').click()
 
+        flashes = driver.find_elements_by_id('flash-messages')
+        assert len(flashes) != 0 and 'An email to reset your password has been sent' in flashes[0].text
+        flashes[0].find_element_by_class_name('close-button').click()
+        time.sleep(5)
 
-def test_l003():
-    driver = webdriver.Chrome()
+        driver.get('https://www.mailinator.com/v2/inbox.jsp?zone=public&query=kumbutest#')
+        driver.find_element_by_xpath('//div[contains(text(), "Reset your Kumbu password")]').click()
 
-    driver.get('https://staging.getkumbu.com')
-    driver.find_element_by_class_name('password-link').click()
-    assert 'https://staging.getkumbu.com/reset' in driver.current_url
+        frame = driver.find_element_by_id('msg-body')
+        driver.switch_to.frame(frame)
+        driver.find_element_by_class_name('mcnButton').click()
 
-    driver.find_element_by_name('inputEmail').send_keys('kumbutest@mailinator.com')
-    driver.find_element_by_id('login-submit').click()
+        driver.switch_to.window(driver.window_handles[1])
+        driver.find_element_by_id('inputPassword').send_keys('$PASSWORD')
+        driver.find_element_by_id('confirmPassword').send_keys('$PASSWORD')
+        driver.find_element_by_id('login-submit').click()
 
-    flashes = driver.find_elements_by_id('flash-messages')
-    assert len(flashes) != 0 and 'An email to reset your password has been sent' in flashes[0].text
-    flashes[0].find_element_by_class_name('close-button').click()
-    time.sleep(5)
+        flashes = driver.find_elements_by_id('flash-messages')
+        assert len(flashes) != 0 and 'Your password has been successfully changed' in flashes[0].text
+        flashes[0].find_element_by_class_name('close-button').click()
 
-    driver.get('https://www.mailinator.com/v2/inbox.jsp?zone=public&query=kumbutest#')
-    driver.find_element_by_xpath('//div[contains(text(), "Reset your Kumbu password")]').click()
+        self.test_l001()
 
-    frame = driver.find_element_by_id('msg-body')
-    driver.switch_to.frame(frame)
-    driver.find_element_by_class_name('mcnButton').click()
+    def test_m001(self):
+        self.test_l001()
 
-    driver.switch_to.window(driver.window_handles[1])
-    driver.find_element_by_id('inputPassword').send_keys('$PASSWORD')
-    driver.find_element_by_id('confirmPassword').send_keys('$PASSWORD')
-    driver.find_element_by_id('login-submit').click()
+        driver = webdriver.Chrome()
+        driver.find_element_by_class_name('souvenirs-menu-link').click()
 
-    flashes = driver.find_elements_by_id('flash-messages')
-    assert len(flashes) != 0 and 'Your password has been successfully changed' in flashes[0].text
-    flashes[0].find_element_by_class_name('close-button').click()
+        prior = 0
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            current = len(WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.item.columns'))))
 
-    test_l001()
+            if current != prior:
+                prior = current
+            else:
+                break
 
+        count = current
 
-def test_m001():
-    test_l001()
+    def test_m002(self):
+        self.test_l001()
 
-    driver = webdriver.Chrome()
-    driver.find_element_by_class_name('souvenirs-menu-link').click()
+        driver = webdriver.Chrome()
+        driver.find_element_by_class_name('souvenirs-menu-link').click()
+        driver.find_element_by_css_selector('ul.dropdown.menu > li').click()
+        driver.find_element_by_class_name('sort-by-title').click()
 
-    prior = 0
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        current = len(WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.item.columns'))))
-
-        if current != prior:
-            prior = current
-        else:
-            break
-
-    count = current
-
-
-def test_m002():
-    test_l001()
-
-    driver = webdriver.Chrome()
-    driver.find_element_by_class_name('souvenirs-menu-link').click()
-    driver.find_element_by_css_selector('ul.dropdown.menu > li').click()
-    driver.find_element_by_class_name('sort-by-title').click()
-
-    '''Not completed'''
+        '''Not completed'''
